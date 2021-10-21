c5e0c9fbbf51d3102195a9edad95f1117f6e0cbe
jude <judeonnadi@gmail.com>
2021-10-21 01:04:38 +0100

    fin

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 let pool;
-if (process.env.NODE_ENV === "developement") {
+if (process.env.NODE_ENV === "production") {
   pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 } else if (process.env.NODE_ENV === "developement") {
   pool = new Pool();
